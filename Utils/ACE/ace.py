Fixed issue with gradients for images with no predictions

Utils/ACE/ace.py
@@ -455,7 +455,7 @@ class ConceptDiscovery(object):
         
         activations_path = self.output_dir / "acts/"
         
-        if not all(self.channel_mean) and self.pca == None and img_paths.shape[0] == 0:
+        if not all(self.channel_mean) and self.pca == None and img_paths is None:
             
             superpixels_dir = self.discovered_concepts_dir / "superpixels"
             img_paths = np.array(list(superpixels_dir.iterdir()))
@@ -514,7 +514,7 @@ class ConceptDiscovery(object):
                 output.append(activations)
             
 
-        elif len(list(activations_path.iterdir())) > 0 and img_paths.shape[0] == 0:
+        elif len(list(activations_path.iterdir())) > 0 and img_paths is None:
             
             activations_path_list = list(activations_path.iterdir())
             
@@ -572,8 +572,8 @@ class ConceptDiscovery(object):
         # Find the number of batches in each to allow for n_components
         # Use divmod to find the number of complete batches, then split the remainder across the batches.
         num_batches, remainder = divmod(img_paths.shape[0], pca_n_components)
-        print(num_batches, remainder)
-        print(pca_n_components, bs)
+#         print(num_batches, remainder)
+#         print(pca_n_components, bs)
 
         # Use the floor rounding so we make sure the last batch always has enough. If we round up and take too much in the
         # first batches we may be left short
@@ -606,7 +606,7 @@ class ConceptDiscovery(object):
 
                 # Take all the batch outputs for that layer and concatenate the results.
                 aggregated_acts = np.concatenate(current_pca_batch)
-                print(aggregated_acts.shape)
+#                 print(aggregated_acts.shape)
                 
                 try:
                     self.pca[bn].partial_fit(aggregated_acts, check_input=False)
@@ -792,7 +792,7 @@ class ConceptDiscovery(object):
         patch_images = np.array(list(patches_dir.iterdir()))
 
         # Get the activations back after passing the superpixels.
-        activations = self._get_activations(np.array(), bs=bs)
+        activations = self._get_activations(None, bs=bs)
 
         # For every bottleneck we will cluster.
         for bn in tqdm(self.bottlenecks, total=len(self.bottlenecks), desc="Clustering to find potential concepts"):
@@ -817,10 +817,7 @@ class ConceptDiscovery(object):
                 if len(label_idxs) > self.min_imgs:
 
                     # Add the details for this cluster to the dic for the current bottleneck layer.
-                    print(len(bn_dic['cost'][label_idxs]))
                     concept_costs = bn_dic['cost'][label_idxs]
-                    print("Label indexes", len(label_idxs))
-                    print("Concept cost", len(concept_costs))
                     concept_idxs = label_idxs[np.argsort(concept_costs)]
                     concept_image_numbers = set([int(p.name.split("_")[0]) for p in patch_images[label_idxs]])
                     highly_common_concept = len(
@@ -1082,6 +1079,9 @@ class ConceptDiscovery(object):
 
             # Pass the image to the get_gradient method and capture the returned gradients and corresponding info.
             img_gradients, detection_info = self.model.get_gradient(img, class_id, self.channel_mean)
+            
+            if img_gradients is None:
+                continue
 
             del img
 
@@ -1094,26 +1094,25 @@ class ConceptDiscovery(object):
             # Iterate through the layers we have and add the corresponding gradients to our total for the layer.
             for layer, vals in img_gradients.items():
                 
-                # Add these gradients to the total we have collected so far
-                gradients[layer].append(final_vals)
+                if not all(self.channel_mean):
+                    gradients[layer].append(self.pca[layer].transform(vals)) 
+                else:
+                
+                    # Add these gradients to the total we have collected so far
+                    gradients[layer].append(vals)
 
-            #
-        if self.channel_mean:
-            final_vals = self.pca[layer].transform(vals)
-        else:
-            final_vals = vals
             
         #Convert the lists to numpy arrays
         for k, v in gradients.items():
             
-            if not all(self.channel_mean):
+#             if not all(self.channel_mean):
             
-                stacked_gradients = np.vstack(v)
+#                 stacked_gradients = np.vstack(v)
 
-                gradients[k] = self.pca[layer].transform(stacked_gradients)
+#                 gradients[k] = self.pca[k].transform(stacked_gradients)
             
-            else:
-                gradients[k] = np.vstack(v)
+#             else:
+            gradients[k] = np.vstack(v)
 
         return gradients, total_info
 

Utils/ACE/ace_helpers.py
@@ -114,17 +114,21 @@ class MyModel():
         del tensor_imgs
         
         gradients, info = self.model.generate_gradients(class_id)
+
+        if len(list(gradients.values())[0]) > 0:
+            flattened = {}
+
+            for k, v, channel_mean in zip(gradients.keys(), gradients.values(), get_mean):
+
+                if channel_mean:
+                    flattened[k] = torch.flatten(torch.mean(v.detach(), dim=1).cpu(), start_dim=1)
+                else:
+                    flattened[k] = torch.flatten(v.detach().cpu(), start_dim=1)
+
+            output = {k: v.numpy() for k, v in flattened.items()}
         
-        flattened = {}
-        
-        for k, v, channel_mean in zip(gradients.keys(), gradients.values(), get_mean):
-            
-            if channel_mean:
-                flattened[k] = torch.flatten(torch.mean(v.detach(), dim=1).cpu(), start_dim=1)
-            else:
-                flattened[k] = torch.flatten(v.detach().cpu(), start_dim=1)
-            
-        output = {k: v.numpy() for k, v in flattened.items()}
+        else:
+            output = None
         
         return output, info
     

Utils/ACE/run_ace.py
@@ -22,92 +22,92 @@ def main():
     # Create the model variable and set it to evaluate.
     mymodel = ace_helpers.MyModel("mitotic", bottleneck_layers)
     
-    tissue_types = ['canine cutaneous mast cell tumor', 'canine lung cancer', 'canine lymphoma', 'human breast cancer', 'human neuroendocrine tumor']
-#     tissue_types = ['canine lung cancer', 'canine lymphoma', 'human breast cancer', 'human neuroendocrine tumor']
+#     tissue_types = ['canine cutaneous mast cell tumor', 'canine lung cancer', 'canine lymphoma', 'human breast cancer', 'human neuroendocrine tumor']
+    tissue_types = ['canine lymphoma', 'human breast cancer', 'human neuroendocrine tumor']
 
    
 
     for tissue_type in tissue_types:
-        try:
+#         try:
         
-            print(tissue_type, "Running")
+        print(tissue_type, "Running")
 
 
-            curr_source_dir = source_dir / tissue_type
+        curr_source_dir = source_dir / tissue_type
 
-            # Create an output directory for our data
-            output = Path("D:/FYP") / f"ACE_mitotis_{tissue_type}/"
+        # Create an output directory for our data
+        output = Path("D:/FYP") / f"ACE_mitotis_{tissue_type}/"
 
-            ace_helpers.create_directories(output, remove_old=False)
+        ace_helpers.create_directories(output, remove_old=False)
 
-            # Creating the ConceptDiscovery class instance.
-            cd = ConceptDiscovery(
-                mymodel,
-                target_class,
-                curr_source_dir,
-                output,
-                bottleneck_layers,
-                num_random_exp=25,
-                channel_mean=[True, True, True, False],
-                min_imgs=50,
-                resize_dims=(512,512),
-                pca_n_components=[600, 200, 100, 600])
+        # Creating the ConceptDiscovery class instance.
+        cd = ConceptDiscovery(
+            mymodel,
+            target_class,
+            curr_source_dir,
+            output,
+            bottleneck_layers,
+            num_random_exp=25,
+            channel_mean=[True, True, True, False],
+            min_imgs=50,
+            resize_dims=(512,512),
+            pca_n_components=[600, 200, 100, 600])
 
-            # Creating the dataset of image patches.
-            patches_created = cd.create_patches(param_dict={'n_segments': [10]})
+        # Creating the dataset of image patches.
+        patches_created = cd.create_patches(param_dict={'n_segments': [10]})
 
-            if patches_created:
-                context_discovery_images = list((curr_source_dir / target_class / "context_discovery").iterdir())
-                # Create the context patches
-                cd.create_patches(param_dict={'n_segments': [4]}, discovery_images=context_discovery_images)
+        if patches_created:
+            context_discovery_images = list((curr_source_dir / target_class / "context_discovery").iterdir())
+            # Create the context patches
+            cd.create_patches(param_dict={'n_segments': [4]}, discovery_images=context_discovery_images)
 
-                ace_helpers.save_discovery_images(cd,save_context=True)
+            ace_helpers.save_discovery_images(cd,save_context=True)
 
-    #         superpixels_dir = cd.discovered_concepts_dir / "superpixels"
-    #         superpixel_images = np.array(list(superpixels_dir.iterdir()))
+#         superpixels_dir = cd.discovered_concepts_dir / "superpixels"
+#         superpixel_images = np.array(list(superpixels_dir.iterdir()))
 
-    #         bs = 2
+#         bs = 2
 
-    #         for bn, pca_n_components in zip(cd.bottlenecks, cd.pca_n_components):
+#         for bn, pca_n_components in zip(cd.bottlenecks, cd.pca_n_components):
 
-    #             if bn in ['backbone.body.layer2.3.conv1', 'backbone.body.layer3.5.conv1'] and tissue_type not in ['canine cutaneous mast cell tumor']:
-    #                 cd.update_pca(bn, pca_n_components, superpixel_images, bs)
+#             if bn in ['backbone.body.layer2.3.conv1', 'backbone.body.layer3.5.conv1'] and tissue_type not in ['canine cutaneous mast cell tumor']:
+#                 cd.update_pca(bn, pca_n_components, superpixel_images, bs)
 
 
-    #             if tissue_type == "canine lymphoma" and bn == "backbone.body.layer4.2.conv1":
-    #                 cd.update_pca(bn, pca_n_components, superpixel_images, bs)
+#             if tissue_type == "canine lymphoma" and bn == "backbone.body.layer4.2.conv1":
+#                 cd.update_pca(bn, pca_n_components, superpixel_images, bs)
 
-            if hasattr(cd, "pca") and cd.pca is not None:
-                check_pca_variance_preserved(cd)
+        if hasattr(cd, "pca") and cd.pca is not None:
+            check_pca_variance_preserved(cd)
 
-            print("Starting concept discovery")
+        print("Starting concept discovery")
 
-            # Discovering Concepts
-            concepts_ran = cd.discover_concepts(method='KM', param_dicts={'n_clusters': 50})
+        # Discovering Concepts
+        concepts_ran = cd.discover_concepts(method='KM', param_dicts={'n_clusters': 50})
 
-            if concepts_ran:
-                print("Saving concepts")
-                # Save discovered concept images (resized and original sized)
-                ace_helpers.save_concepts(cd)
+        if concepts_ran:
+            print("Saving concepts")
+            # Save discovered concept images (resized and original sized)
+            ace_helpers.save_concepts(cd)
 
-            print("Initializing random concepts")
-            cd.initialize_random_concept_and_samples()
+        print("Initializing random concepts")
+        cd.initialize_random_concept_and_samples()
 
-            print("Computing CAVs")
-            cav_accuracies = cd.cavs()
+        print("Computing CAVs")
+        cav_accuracies = cd.cavs()
 
-            print("Computing TCAV scores")
-            scores = cd.tcavs(test=False, sort=False)
+        print("Computing TCAV scores")
+        scores = cd.tcavs(test=False, sort=False)
 
-            print("Generating report")
-            cd.save_ace_report()
+        print("Generating report")
+        cd.save_ace_report()
 
-            # Plot examples of discovered concepts
-            for bn in cd.bottlenecks:
-                cd.plot_concepts(bn, 10)
+        # Plot examples of discovered concepts
+        for bn in cd.bottlenecks:
+            cd.plot_concepts(bn, 10)
                 
-        except Exception as e:
-            print(e)
+#         except Exception as e:
+#             print(e)
 
 
 def check_pca_variance_preserved(cd):